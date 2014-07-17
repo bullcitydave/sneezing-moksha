7cfada561b4e5dbc708ef6eb83baae823bfa008c
Dave Seidman <bullcitydave@gmail.com>
2014-07-16 21:34:33 -0400

    Working on initial template...but with underscore as instructed...more cumbersome

--- app/scripts/views.js
+++ app/scripts/views.js
@@ -1,16 +1,16 @@
 var AdoptablePetListView = Backbone.View.extend({
 
     initialize: function(){
-        this.listenTo(this.collection, 'add', this.render);
-        this.listenTo(this.collection, 'change', this.render);
-        this.listenTo(this.collection, 'remove', this.render);
-        this.collection.fetch();
+        // this.listenTo(this.collection, 'add', this.render);
+        // this.listenTo(this.collection, 'change', this.render);
+        // this.listenTo(this.collection, 'remove', this.render);
+        // this.collection.fetch();
       },
 
     render: function(){
         var source = $('#pet-list-template').html();
-        var template = Handlebars.compile(source);
-        var rendered = template({ourPets: this.collection.toJSON()})
+        // var rendered = _.template(source, {ourPets: this.collection.toJSON()})\
+        var rendered = _.template(source,{name : 'Moksha'});
         this.$el.append(rendered);
         return this;
       },
